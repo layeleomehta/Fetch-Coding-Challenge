a531786b08f918a7c8d9cb72358f54cacfa546bd
Laye Mehta <layeamehta@gmail.com>
2023-03-04 23:36:13 -0700

    created receipt service with functionality to create receipt

--- receipt-processor/src/main/kotlin/com/fetch/challenge/receiptprocessor/services/ReceiptService.kt
+++ receipt-processor/src/main/kotlin/com/fetch/challenge/receiptprocessor/services/ReceiptService.kt
@@ -1,2 +1,30 @@
 package com.fetch.challenge.receiptprocessor.services
 
+import com.fetch.challenge.receiptprocessor.database.dao.ReceiptRepository
+import com.fetch.challenge.receiptprocessor.database.models.Receipt
+import org.springframework.stereotype.Service
+
+@Service
+class ReceiptService(
+    private val idGeneratorService: IdGeneratorService,
+    private val receiptRepository: ReceiptRepository
+) {
+    fun createReceipt(
+        retailer: String,
+        purchaseDate: String,
+        purchaseTime: String,
+        total: Float
+    ): Receipt {
+        return receiptRepository.save(
+            Receipt(
+                null,
+                idGeneratorService.generateId(),
+                retailer,
+                purchaseDate,
+                purchaseTime,
+                total,
+                emptySet()
+            )
+        )
+    }
+}
