00e4d5eac3ab187f2e800f6b94b2bc599cedca74
Laye Mehta <layeamehta@gmail.com>
2023-03-04 17:58:08 -0700

    fixed classpath errors, created project with jdk 11 and spring boot v2

--- receipt-processor/build.gradle.kts
+++ receipt-processor/build.gradle.kts
@@ -1,37 +1,37 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.4"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
-	kotlin("plugin.jpa") version "1.7.22"
+	id("org.springframework.boot") version "2.7.10-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.fetch.challenge"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.springframework.integration:spring-integration-core")
 	implementation("org.flywaydb:flyway-core")
-	runtimeOnly("org.postgresql:postgresql")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+		jvmTarget = "11"
 	}
 }
 
--- receipt-processor/src/main/resources/db/migration/V1.0__initial_create_tables.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE chat_user
-(
-	id              SERIAL PRIMARY KEY,
-	external_id     VARCHAR(128) UNIQUE NOT NULL,
-	username        VARCHAR(128) NOT NULL
-);
