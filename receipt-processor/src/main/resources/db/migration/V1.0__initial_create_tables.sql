efb1282ec9f5732fd6b4c152f34b9fd43d5f8b74
Laye Mehta <layeamehta@gmail.com>
2023-03-04 18:27:57 -0700

    flyway config, and database sql scripts

---
+++ receipt-processor/src/main/resources/db/migration/V1.0__initial_create_tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE receipt
+(
+	id              SERIAL PRIMARY KEY,
+	external_id     VARCHAR(128) UNIQUE NOT NULL,
+	retailer        VARCHAR(256) NOT NULL,
+	purchase_date   VARCHAR(256) NOT NULL,
+	purchase_time   VARCHAR(256) NOT NULL,
+	total           FLOAT NOT NULL
+);
+
+CREATE TABLE item
+(
+	id                         SERIAL PRIMARY KEY,
+	external_id                VARCHAR(128) UNIQUE NOT NULL,
+	receipt_id                 INTEGER NOT NULL,
+	short_description          TEXT NOT NULL,
+	price                      FLOAT NOT NULL,
+	FOREIGN KEY (receipt_id)   REFERENCES receipt(id)
+);
+
+CREATE TABLE points
+(
+	id                         SERIAL PRIMARY KEY,
+	external_id                VARCHAR(128) UNIQUE NOT NULL,
+	receipt_id                 INTEGER NOT NULL,
+	points                     INTEGER NOT NULL,
+	FOREIGN KEY (receipt_id)   REFERENCES receipt(id)
+);
