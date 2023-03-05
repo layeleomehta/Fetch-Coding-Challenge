e90502eda87df628afd11918b2bde05812db3976
Laye Mehta <layeamehta@gmail.com>
2023-03-04 23:43:12 -0700

    created item service with ability to create item referring to specific receipt

---
+++ receipt-processor/src/main/kotlin/com/fetch/challenge/receiptprocessor/services/ItemService.kt
@@ -0,0 +1,25 @@
+package com.fetch.challenge.receiptprocessor.services
+
+import com.fetch.challenge.receiptprocessor.database.dao.ItemRepository
+import com.fetch.challenge.receiptprocessor.database.models.Item
+import com.fetch.challenge.receiptprocessor.database.models.Receipt
+import org.springframework.stereotype.Service
+
+@Service
+class ItemService(
+    private val idGeneratorService: IdGeneratorService,
+    private val itemRepository: ItemRepository
+) {
+    fun createItem(shortDescription: String, price: Float, receipt: Receipt): Item {
+        return itemRepository.save(
+            Item(
+                null,
+                idGeneratorService.generateId(),
+                receipt,
+                shortDescription,
+                price
+            )
+        )
+    }
+
+}
