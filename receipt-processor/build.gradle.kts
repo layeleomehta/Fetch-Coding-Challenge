ee25ec9e8dcf59bbdd838e1b460daefa1e38170b
Laye Mehta <layeamehta@gmail.com>
2023-03-06 13:20:16 -0700

    successful gradlew build and added appengine deployment config

--- receipt-processor/build.gradle.kts
+++ receipt-processor/build.gradle.kts
@@ -1,11 +1,25 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+
+	dependencies {
+		classpath("com.google.cloud.tools:appengine-gradle-plugin:2.+")
+	}
+}
+
 plugins {
 	id("org.springframework.boot") version "2.7.10-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
+}
+
+apply {
+	plugin("com.google.cloud.tools.appengine")
 }
 
 group = "com.fetch.challenge"
@@ -24,8 +38,15 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.google.cloud.sql:postgres-socket-factory:1.4.2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+configure<com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension> {
+	stage {
+		setArtifact("build/libs/receipt-processor-0.0.1-SNAPSHOT.jar")
+	}
 }
 
 tasks.withType<KotlinCompile> {
